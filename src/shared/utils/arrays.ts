a554a3aee61840e3e9ac32ebc650831b5bd32806
Ben <codeandcats@gmail.com>
2017-11-13 15:41:41 +1100

    Add my classic compareArrays algorithm

--- src/shared/utils/arrays.ts
+++ src/shared/utils/arrays.ts
@@ -21,3 +21,102 @@ export function moveElement<T>(items: T[], fromIndex: number, toIndex: number): 
 
 	return items;
 }
+
+export interface CompareArraysOptions<TLeft, TRight> {
+	left: TLeft[];
+	right: TRight[];
+	compare: (left: TLeft, right: TRight) => number;
+}
+
+export interface CompareArrayOptionsWithEvents<TLeft, TRight> extends CompareArraysOptions<TLeft, TRight> {
+	missingInLeft?: (right: TRight) => void;
+	missingInRight?: (left: TLeft) => void;
+	matched?: (left: TLeft, right: TRight) => void;
+}
+
+/**
+ * Compares 2 arrays, both in ascending order, and reports back on missing and matched items.
+ * Note that this function will not work correctly unless the arrays you provide are both in ascending order.
+ * Implementation deliberately avoids adding automatic sorting/checking for performance, so its up to the
+ * caller to get it right.
+ */
+export function compareArrays<TLeft, TRight>(options: CompareArrayOptionsWithEvents<TLeft, TRight>): void {
+	const { left, right, compare, missingInLeft, missingInRight, matched } = options;
+
+	const leftLength = left.length;
+	const rightLength = right.length;
+
+	let leftIndex = 0;
+	let rightIndex = 0;
+
+	const leftFinished = () => leftIndex >= leftLength;
+	const rightFinished = () => rightIndex >= rightLength;
+
+	while (!leftFinished() || !rightFinished()) {
+		const leftItem = left[leftIndex];
+		const rightItem = right[rightIndex];
+
+		const result =
+			leftFinished() ? 1 :
+				rightFinished() ? -1 :
+					compare(leftItem, rightItem);
+
+		if (result < 0) {
+			if (missingInRight) {
+				missingInRight(leftItem);
+			}
+			leftIndex++;
+		} else if (result > 0) {
+			if (missingInLeft) {
+				missingInLeft(rightItem);
+			}
+			rightIndex++;
+		} else {
+			if (matched) {
+				matched(leftItem, rightItem);
+			}
+			leftIndex++;
+			rightIndex++;
+		}
+	}
+}
+
+export interface CompareArraysResults<TLeft, TRight> {
+	missingInLeft: TRight[];
+	missingInRight: TLeft[];
+	matched: Array<{ left: TLeft, right: TRight }>;
+}
+
+export function compareArraysWithResults<TLeft, TRight>(
+	options: CompareArraysOptions<TLeft, TRight>
+): CompareArraysResults<TLeft, TRight> {
+	const result = {
+		missingInLeft: [] as TRight[],
+		missingInRight: [] as TLeft[],
+		matched: [] as Array<{ left: TLeft, right: TRight }>
+	};
+
+	compareArrays({
+		...options,
+		missingInLeft: item => result.missingInLeft.push(item),
+		missingInRight: item => result.missingInRight.push(item),
+		matched: (left, right) => result.matched.push({ left, right })
+	});
+
+	return result;
+}
+
+export class DefaultMap<K, V> extends Map<K, V> {
+	constructor(protected readonly defaultFactory: (key: K) => V, entries?: Array<[K, V]>) {
+		super(entries);
+	}
+
+	public get(key: K): V {
+		let value = super.get(key);
+		if (value === undefined) {
+			value = this.defaultFactory(key);
+			this.set(key, value);
+		}
+		return value;
+	}
+}
