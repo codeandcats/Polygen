9b168cf069338242591f67e515069dead50d16f8
Ben <codeandcats@gmail.com>
2017-11-13 15:41:08 +1100

    Fix bug where closing a file could result in a completely blank ui

--- src/ui/actions/editor/closeActiveProjectFile.ts
+++ src/ui/actions/editor/closeActiveProjectFile.ts
@@ -3,9 +3,12 @@ import { defineAction } from '../../reduxWithLessSux/action';
 
 export const closeActiveProjectFile = defineAction(
 	'closeActiveProjectFile', (state: ApplicationState) => {
-		const editors = state.editors.filter((_, editorIndex) => editorIndex !== state.activeEditorIndex);
+		let { activeEditorIndex } = state;
+		const editors = state.editors.filter((_, editorIndex) => editorIndex !== activeEditorIndex);
+		activeEditorIndex = editors.length - 1;
 		return {
 			...state,
+			activeEditorIndex,
 			editors
 		};
 	}
