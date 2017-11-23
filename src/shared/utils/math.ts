c69ce21b01a00a3dd6f47271318a38d9c7de5c2a
Ben <codeandcats@gmail.com>
2017-11-23 14:43:00 +1100

    Mod function that handles negatives properly

--- src/shared/utils/math.ts
+++ src/shared/utils/math.ts
@@ -1,3 +1,7 @@
 export function clamp(min: number, max: number, value: number): number {
 	return Math.max(min, Math.min(max, value));
 }
+
+export function mod(value: number, divisor: number) {
+	return ((value % divisor) + divisor) % divisor;
+}
